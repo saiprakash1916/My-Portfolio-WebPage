7ba36957fc2118cae9683b45da789b683a685c7c
Sai Prakash <saiprakash1916@gmail.com>
2022-12-13 22:14:37 +0530

    About me page was updated

--- src/Compoments/About/About.jsx
+++ src/Compoments/About/About.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import './About.css'
+import ME from '../../assets/SaiAbout.png'
+import {FiAward, FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='About'>About</section>
+    <section id='About'>
+        <h5>Go to Know</h5>
+        <h2>About Me</h2>
+        <div className='container about__container'>
+            <div className='about__me'>
+                <div className='about__me-image'>
+                    <img src={ME} alt="About me picture"/>
+                </div>
+            </div>
+            <div className='about__content'>
+                <div className='about__cards'>
+                  <article className='about__card'>
+                      <FiAward className='about__icon'/>
+                      <h5>Experience</h5>
+                      <small>1.7+ years Working</small>
+                  </article>
+                  <article className='about__card'>
+                      <FiUsers className='about__icon'/>
+                      <h5>Clients</h5>
+                      <small>10 + Worldwide</small>
+                  </article>
+                  <article className='about__card'>
+                      <VscFolderLibrary className='about__icon'/>
+                      <h5>Projects</h5>
+                      <small>2 Completed</small>
+                  </article>
+                </div>
+                <p>Proven as Associate Engineer with 1.4+ years of experience in Development. Experienced in Designing the Web Applications based on the requirement. Proven skills in completing Black and White box Testing.</p>
+                <a href="#Conatct" className='btn btn-primary'>Let's Talk</a>
+            </div>
+        </div>
+    </section>
   )
 }
 
--- src/Compoments/Experience/Experience.jsx
+++ src/Compoments/Experience/Experience.jsx
@@ -1,9 +1,95 @@
 import React from 'react'
 import './Experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='Experience'>Experience</section>
+    <section id='Experience'>
+        <h5>What Skills I Have</h5>
+        <h2>Experience</h2>
+        <div className='container experience__container'>
+            <div className='experience__frontend'>
+              <h3>Frontend Developemt</h3>
+              <div className='experience__content'> 
+                  <article className='experience__details'>
+                    <BsPatchCheckFill className='.experience__details-icons'/>
+                    <div>
+                      <h4>HTML</h4>
+                      <small className='text-light'>Experienced</small>
+                    </div>
+                  </article>
+                  <article className='experience__details'>
+                    <BsPatchCheckFill className='.experience__details-icons'/>
+                    <div>
+                      <h4>CSS</h4>
+                      <small className='text-light'>Intermediate</small>
+                    </div>
+                  </article>
+                  <article className='experience__details'>
+                    <BsPatchCheckFill className='.experience__details-icons'/>
+                    <div>
+                      <h4>BootStrap</h4>
+                      <small className='text-light'>Intermediate</small>
+                    </div>
+                  </article>
+                  <article className='experience__details'>
+                    <BsPatchCheckFill className='.experience__details-icons'/>
+                    <div>
+                      <h4>JavaScript</h4>
+                      <small className='text-light'>Intermediate</small>
+                    </div>
+                  </article>
+                  <article className='experience__details'>
+                    <BsPatchCheckFill className='.experience__details-icons'/>
+                    <div>
+                      <h4>React Js</h4>
+                      <small className='text-light'>Intermediate</small>
+                    </div>
+                  </article>
+              </div>
+            </div>
+            <div className='experience__backend'>
+              <h3>Backend Developemt</h3>
+              <div className='experience__content'> 
+                  <article className='experience__details'>
+                    <BsPatchCheckFill className='.experience__details-icons'/>
+                    <div>
+                      <h4>Core Java</h4>
+                      <small className='text-light'>Intermediate</small>
+                    </div>
+                  </article>
+                  <article className='experience__details'>
+                    <BsPatchCheckFill className='.experience__details-icons'/>
+                    <div>
+                      <h4>JDBC</h4>
+                      <small className='text-light'>Intermediate</small>
+                    </div>
+                  </article>
+                  <article className='experience__details'>
+                    <BsPatchCheckFill className='.experience__details-icons'/>
+                    <div>
+                      <h4>JSP & Servlets</h4>
+                      <small className='text-light'>Intermediate</small>
+                    </div>
+                  </article>
+                  <article className='experience__details'>
+                    <BsPatchCheckFill className='.experience__details-icons'/>
+                    <div>
+                      <h4>Spring</h4>
+                      <small className='text-light'>Intermediate</small>
+                    </div>
+                  </article>
+                  <article className='experience__details'>
+                    <BsPatchCheckFill className='.experience__details-icons'/>
+                    <div>
+                      <h4>Database MySql</h4>
+                      <small className='text-light'>Intermediate</small>
+                    </div>
+                  </article>
+              </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
