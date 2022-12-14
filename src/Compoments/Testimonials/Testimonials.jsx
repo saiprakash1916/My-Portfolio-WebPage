d0b4c072f165989914b79bc3899977abd75bcae3
Sai Prakash <saiprakash1916@gmail.com>
2022-12-14 21:59:37 +0530

    Updated the services and some code changes

--- src/Compoments/Testimonials/Testimonials.jsx
+++ src/Compoments/Testimonials/Testimonials.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './Testimonials.css'
+import AVT1 from '../../assets/avatar1.jpg'
+import AVT2 from '../../assets/avatar2.jpg'
+import AVT3 from '../../assets/avatar3.jpg'
+import AVT4 from '../../assets/avatar4.jpg'
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section id='Testimonials'>
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className='container testimonials__container'>
+        <article className='testimonials__'>
+          <div className='client__avatar'>
+            <img src="" alt=""/>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
